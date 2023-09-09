ba121d08e7b41e922d9169b0f75f1602cc218398
tombealby <tom.bealby67@gmail.com>
2023-09-09 18:12:23 +0100

    Add initial tests for Sniper Controller.

--- src/test/java/bealby/tom/AuctionSniper/controller/AuctionControllerTest.java
+++ src/test/java/bealby/tom/AuctionSniper/controller/AuctionControllerTest.java
@@ -11,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,6 +26,8 @@ public class AuctionControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    private RestTemplate restTemplate = new RestTemplate();
+
 	@Test
 	public void joinAuctionShouldChangeSniperStatus() throws Exception {
 
@@ -32,7 +37,6 @@ public class AuctionControllerTest {
 	}
 
 	private void thenTheSniperStatusIsChangedToBiddingStarted() throws UnsupportedEncodingException, Exception {
-		// THEN sniper status is changed to
         String finalStatus = mockMvc.perform(get(GET_STATUS)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -43,7 +47,6 @@ public class AuctionControllerTest {
 	}
 
 	private void whenARequestIsMadeToJoinTheAuction() throws UnsupportedEncodingException, Exception {
-		//WHEN the sniper makes a request to join auction
         String content = mockMvc.perform(get(START_BIDDING)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -62,6 +65,71 @@ public class AuctionControllerTest {
         System.out.println("intialStatus:" + intialStatus);
 
         assertEquals("current status:NotStartedBidding" , intialStatus);
+	}
+
+	@Test
+	public void sniperJoinsAuctionUntilAuctionClose() throws UnsupportedEncodingException, Exception {
+		givenAuctionIsSellingItem();
+		whenARequestIsMadeToJoinTheAuction();
+		thenAuctionHasReceivedRequestFromSniper();
+		whenAuctionCloses(); // fails here - Auction can't connect back to the Sniper
+		thenSniperHasLostAuction();
+	}
+
+	private void thenSniperHasLostAuction() throws UnsupportedEncodingException, Exception {
+		sniperKnowsAuctionIsClosed();
+		// TODO implement the following
+//		sniperKnowsItDidNotWin();
+	}
+
+	private void sniperKnowsAuctionIsClosed() throws UnsupportedEncodingException, Exception {
+        String finalStatus = mockMvc.perform(get(GET_STATUS)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        System.out.println("intialStatus:" + finalStatus);
+
+        assertEquals("current status:biddingStarted" , finalStatus);
+	}
+
+	private void whenAuctionCloses() {
+		closeAuction();
+	}
+
+	private void thenAuctionHasReceivedRequestFromSniper() {
+		ResponseEntity<String> response = getAuctionReceiveStatus();
+		String actualResponse = response.getBody();
+		String expectedResponse = "ReceiveStatus:true";
+		assertEquals(expectedResponse, actualResponse);
+	}
+
+	private void givenAuctionIsSellingItem() {
+		ResponseEntity<String> response = getAuctionStatus();
+		String actualResponse = response.getBody();
+		String expectedResponse = "current status:Open";
+		assertEquals(expectedResponse, actualResponse);
+	}
+
+	private ResponseEntity<String> getAuctionStatus() throws HttpClientErrorException {
+	    final String url = "http://localhost:8093/getStatus";
+		return restTemplate.getForEntity(url, String.class);
+	}
+
+	private ResponseEntity<String> getAuctionReceiveStatus() throws HttpClientErrorException {
+	    final String url = "http://localhost:8093/getReceiveStatus";
+		return restTemplate.getForEntity(url, String.class);
+	}
+
+	private ResponseEntity<String> closeAuction() throws HttpClientErrorException {
+	    final String url = "http://localhost:8093/closeAuction";
+		return restTemplate.getForEntity(url, String.class);
+
+		// Auction throws error when this method is called ..
+
+//		notify clients that auction is closed
+//		2023-09-09 18:06:08.565 ERROR 27549 --- [nio-8093-exec-5] o.a.c.c.C.[.[.[/].[dispatcherServlet]    : Servlet.service() for servlet [dispatcherServlet] in context with path [] threw exception [Request processing failed; nested exception is org.springframework.web.client.ResourceAccessException: I/O error on GET request for "http://localhost:8092/receiveAuctionMessage": Connection refused (Connection refused); nested exception is java.net.ConnectException: Connection refused (Connection refused)] with root cause
+//
+//		java.net.ConnectException: Connection refused (Connection refused)
 	}
 
 }
