ebcdfb34e4e8dfc86767d8e0e78c40c4ced1a787
tombealby <tom.bealby67@gmail.com>
2023-09-09 16:28:11 +0100

    Initial maven project setup.

---
+++ src/main/java/bealby/tom/AuctionSniper/AuctionSniperApplication.java
@@ -0,0 +1,13 @@
+package bealby.tom.AuctionSniper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionSniperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionSniperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bealby/tom/AuctionSniper/AuctionSniperApplicationTests.java
@@ -0,0 +1,13 @@
+package bealby.tom.AuctionSniper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionSniperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
