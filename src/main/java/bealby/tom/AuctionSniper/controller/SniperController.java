bd3781cd0be0c72736110a808afde6506edf703b
tombealby <tom.bealby@outlook.com>
2023-10-22 10:58:43 +0100

    Provide endpoint to force reset of state to initial pre-auction-join state.

--- src/main/java/bealby/tom/AuctionSniper/controller/SniperController.java
+++ src/main/java/bealby/tom/AuctionSniper/controller/SniperController.java
@@ -8,13 +8,18 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-
+// TODO does this class need refactoring to move business logic into a service?
+// at the moment it handles both endpoint/interface logic and business logic.
 @RestController
 public class SniperController {
 	
-	private String status = "NotStartedBidding";
+	private static final int BID_VALUE_WHEN_NOT_STARTED_BIDDING = -1;
+	private static final String STATUS_WHEN_NOT_STARTED_BIDDING = "NotStartedBidding";
+
+	private String status = STATUS_WHEN_NOT_STARTED_BIDDING;
+	private int myLatestBid = BID_VALUE_WHEN_NOT_STARTED_BIDDING;
+
 	private RestTemplate restTemplate = new RestTemplate();
-	private int myLatestBid;
 
 	@RequestMapping("/startBidding")
 	public ResponseEntity<String> startBidding() {
@@ -39,7 +44,7 @@ public class SniperController {
 		status = "Lost";
 		return ResponseEntity.ok("Received a message from the auction");
 	}
-	
+
 	@RequestMapping("/getStatus")
 	public ResponseEntity<String> getStatus() {
 		System.out.println("Received a request to show my status. My current status is:" + status);
@@ -83,6 +88,14 @@ public class SniperController {
 	public ResponseEntity<String> getLatestBid() {
 		System.out.println("Received a request to show my latest bid. My latest bid is:" + myLatestBid);
 		return ResponseEntity.ok("My latest bid:" + myLatestBid);
+	}
+
+	@RequestMapping("/resetState")
+	public ResponseEntity<String> resetMyState() {
+		System.out.println("Received a request to reset my state to initial state");
+		this.status = STATUS_WHEN_NOT_STARTED_BIDDING;
+		this.myLatestBid = BID_VALUE_WHEN_NOT_STARTED_BIDDING;
+		return ResponseEntity.ok("My state has been " + myLatestBid);
 	}
 
 }
